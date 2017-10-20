9b1799414381cf67245e057458215aa44ea07815
Partizan-LERO <corazonsergey@gmail.com>
2017-10-20 03:07:44 +0300

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@ Auth::routes();
 
 Route::get('/', 'PostsController@index')->name('index');
 Route::get('post/{id}', 'PostsController@getPost')->name('post');
-Route::get('create', 'PostsController@create')->name('create');
-Route::get('update/{id}', 'PostsController@update')->name('update');
-Route::get('delete/{id}', 'PostsController@delete')->name('delete');
-Route::post('store', 'PostsController@store')->name('store');
-Route::post('edit/{id}', 'PostsController@edit')->name('edit');
-Route::get('/tinymce_example', function () {
-    return view('mceImageUpload::example');
+
+Route::group(['middleware' => 'auth'], function()
+{
+    Route::get('create', 'PostsController@create')->name('create');
+    Route::get('update/{id}', 'PostsController@update')->name('update');
+    Route::get('delete/{id}', 'PostsController@delete')->name('delete');
+    Route::post('store', 'PostsController@store')->name('store');
+    Route::post('edit/{id}', 'PostsController@edit')->name('edit');
+    Route::get('/tinymce_example', function () {
+        return view('mceImageUpload::example');
+    });
 });
